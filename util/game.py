5bb72cba100233c8b0c154f8e15bb4f839a0756f
inesacosic <7inesacosic7@gmail.com>
2025-02-25 18:56:42 -0500

    attempt at creating game server

---
+++ util/game.py
@@ -0,0 +1,16 @@
+"""import template chat"""
+from llm_utils import TemplateChat
+from base import DungeonMaster
+from base import Player
+
+def run_console_chat(**kwargs):
+    template_file = 'util/templates/dm_chat.json'
+    chat = TemplateChat.from_file(template_file, **kwargs)
+
+
+run_console_chat()
+DM = DungeonMaster()
+DM.start_server()
+DM.server.accept_clients()
+player_1 = Player('inesa')
+player_1.connect()
