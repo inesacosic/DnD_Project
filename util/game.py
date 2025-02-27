1e01cdd6b8a06e64411bc37260448d666037635a
inesacosic <7inesacosic7@gmail.com>
2025-02-27 16:35:44 -0500

    keep server running

--- util/game.py
+++ util/game.py
@@ -3,6 +3,7 @@
 from llm_utils import TemplateChat
 from base import DungeonMaster
 from base import Player
+import time
 
 def run_console_chat(**kwargs):
     template_file = 'util/templates/dm_chat.json'
@@ -15,6 +16,8 @@ DM = DungeonMaster()
 ## starts a background thread that accepts clients 
 DM.start_server()
 
-player_1 = Player('inesa')
-player_1.connect()
+while True:
+    time.sleep(1)
+
+
 
