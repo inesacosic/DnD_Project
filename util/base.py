892d9a943d0f2d62e38c64211037b074614389eb
inesacosic <7inesacosic7@gmail.com>
2025-04-18 21:34:00 -0400

    first attempt at switching prompts

--- util/base.py
+++ util/base.py
@@ -22,10 +22,8 @@ class DungeonMaster:
         # Do DM things here. You can use self.game_log to access the game log
 
         if self.start:
-
-            # store the parameters 
-            self.chat.parameters |= {'context': self.get_latest_game()} # latest game
-
+            # check if there was a previous game and load it into parameters
+            self.chat.parameters |= {'context': self.get_latest_game()} 
             # store latest game as context in messages
             for item in self.chat.messages:
                 item['content'] = insert_params(item['content'], **self.chat.parameters)
@@ -33,8 +31,15 @@ class DungeonMaster:
             dm_message = self.chat.start_chat()
             self.start = False
         else: 
-
-            # insert the current players names into the name parameter so DM can address current player by name
+            if self.server.switch is True:
+                selected = self.switch_prompts()
+                dm_message = TemplateChat.from_file(selected['as'] + '_chat.json', sign = 'hello').completion({
+                'action': selected['action'],
+                'task': selected['task']})
+                self.server.switch = False
+                return dm_message
+            
+            # insert the current players name into the name parameter so DM can address current player by name
             params = {'name': self.server.current_client}
             for item in self.chat.messages:
                 item['content'] = insert_params(item['content'], **params)
@@ -45,12 +50,12 @@ class DungeonMaster:
         return dm_message 
     
     def get_latest_game(self):
-
-        now = int(datetime.datetime.now().timestamp()) # current date
-        two_weeks_ago = now - 14 * 24 * 60 * 60 # date two weeks ago
-
+        # get current date and date two weeks ago
+        now = int(datetime.datetime.now().timestamp()) 
+        two_weeks_ago = now - 14 * 24 * 60 * 60 
+        # get the collections where the date is greater than two weeks ago
         results = self.server.collection.get(
-            where = {"date": {"$gt": two_weeks_ago}} # get the collections where the date is greater than two weeks ago
+            where = {"date": {"$gt": two_weeks_ago}}
         )
 
         if not results['metadatas']:
@@ -63,26 +68,33 @@ class DungeonMaster:
             key=lambda x: x[0]["date"],
             reverse=True
         )
-
+        # return document portion of latest game
         latest_document = sorted_data[0][1]
+
         return latest_document
+    
+    def switch_prompts(self):
+        selection = TemplateChat.from_file('util/templates/selection_chat.json', sign = 'hello')
+        # get the last 5 turns from the game log and insert into parameters
+        self.selection.parameters  |= {{'ask': " ".join(self.game_log[-5:])}}
+        for item in self.selection.messages:
+            item['user'] = insert_params(item['user'], **self.chat.parameters)
+        response = selection.completion() 
+        selected = json.loads(response.choices[0].message.content)
+        # return to selected prompt
+        return selected
+
 
 
     def create_character(character_class):
         # Use API for character creation based on chosen class
         url = f"https://www.dnd5eapi.co/api/2014/classes/{character_class}"
-
-
         payload = {}
         headers = {
         'Accept': 'application/json'
         }
-
-
         response = requests.request("GET", url, headers=headers, data=payload)
-
         data = json.loads(response.text)
-
         print(data)
 
 
